e243b3e12db0df5d237487fd68c4b645fc52373a
VIGGIANO <antoine.viggiano@isen.yncrea.fr>
2020-01-20 17:23:23 +0100

    Avec la correction du cycle de vie

---
+++ app/src/main/java/fr/isen/viggiano/androidtoolbox/LifeCycleActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.viggiano.androidtoolbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LifeCicleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_life_cicle)
+    }
+}
